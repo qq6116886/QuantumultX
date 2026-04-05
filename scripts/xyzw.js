f252e44e4afd1d8a5a0a4ec0793b69cc25bf268a
qq6116886 <57084590+qq6116886@users.noreply.github.com>
2026-04-05 21:26:55 +0800

    Update xyzw.js

--- scripts/xyzw.js
+++ scripts/xyzw.js
@@ -12,7 +12,7 @@
 *******************************
 [rewrite_local]
 # >咸鱼之王 - 通用配置修改（敌伤害=1,血量=1）
-^https?:\/\/xxz-xyzw-res\.hortorgames\.com\/remote\/config\/import\/10 url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/xyzw.js
+^https?:\/\/xxz-xyzw-res\.hortorgames\.com\/remote\/config\/import\/10 url script-response-body https://raw.githubusercontent.com/qq6116886/QuantumultX/refs/heads/main/scripts/xyzw.js
 
 [mitm]
 hostname = xxz-xyzw-res.hortorgames.com
